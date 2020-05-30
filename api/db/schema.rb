a215832edd21e67ea8d4b76e961fa2d6874ba27c
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2020-05-30 17:31:26 +0200

    Basic devise-jwt configuration with blacklist

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_111707) do
+ActiveRecord::Schema.define(version: 2020_05_30_145954) do
+
+  create_table "jwt_blacklist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["jti"], name: "index_jwt_blacklist_on_jti"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
